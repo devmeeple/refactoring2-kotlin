f228ffe557b1a853fb2a6d27d25b21b080f61dcc
장태근 <devmeeple@gmail.com>
2025-01-19 14:51:23 +0900

    Kotest, Serialization 추가
    - object를 사용한 버전 관리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "io.github.devmeeple"
 version = "1.0-SNAPSHOT"
+
+object Versions {
+    const val KOTEST = "5.9.1"
+    const val KOTLINX_SERIALIZATION = "1.8.0"
+}
 
 repositories {
     mavenCentral()
@@ -11,6 +17,13 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    // Kotest Test Framework + Assertions
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:${Versions.KOTEST}")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:${Versions.KOTEST}")
+
+    // Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.KOTLINX_SERIALIZATION}")
 }
 
 tasks.test {
