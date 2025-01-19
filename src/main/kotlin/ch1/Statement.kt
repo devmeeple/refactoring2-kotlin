d52ac22a6e59449b118a1ca94ceb46ef71f6b234
장태근 <devmeeple@gmail.com>
2025-01-19 23:13:12 +0900

    [1-5] 중간 점검: 난무하는 중첩 함수
    - 책과 동일한 예제 작성을 위해 의도적으로 중첩 함수 사용

--- src/main/kotlin/ch1/Statement.kt
+++ src/main/kotlin/ch1/Statement.kt
@@ -4,10 +4,40 @@ import java.text.NumberFormat
 import java.util.*
 
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
-    var result = "청구 내역 (고객명: ${invoice.customer})\n"
+    fun playFor(performance: Performance) = ch1.plays[performance.playID]!!
 
-    invoice.performances.forEach { perf ->
-        result += " ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n"
+    fun amountFor(performance: Performance): Int {
+        var result: Int
+
+        when (playFor(performance).type) {
+            "tragedy" -> { // 비극
+                result = 40000
+                if (performance.audience > 30) {
+                    result += 1000 * (performance.audience - 30)
+                }
+            }
+
+            "comedy" -> { // 희극
+                result = 30000
+                if (performance.audience > 20) {
+                    result += 10000 + 500 * (performance.audience - 20)
+                }
+                result += 300 * performance.audience
+            }
+
+            else -> {
+                throw IllegalArgumentException("알 수 없는 장르: ${playFor(performance).type}")
+            }
+        }
+        return result
+    }
+
+    fun volumeCreditsFor(perf: Performance): Int {
+        var result = 0
+        result += maxOf(perf.audience - 30, 0)
+        if ("comedy" == playFor(perf).type) result += perf.audience / 5
+
+        return result
     }
 
     fun totalAmount(): Int {
@@ -26,47 +56,17 @@ fun statement(invoice: Invoice, plays: Map<String, Play>): String {
         return result
     }
 
+    fun usd(number: Int): String {
+        return NumberFormat.getCurrencyInstance(Locale.US).apply { minimumFractionDigits = 2 }.format(number / 100)
+    }
+
+    var result = "청구 내역 (고객명: ${invoice.customer})\n"
+
+    invoice.performances.forEach { perf ->
+        result += " ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n"
+    }
+
     result += "총액: ${usd(totalAmount())}\n"
     result += "적립 포인트: ${totalVolumeCredits()}점\n"
-    return result
-}
-
-private fun usd(number: Int): String {
-    return NumberFormat.getCurrencyInstance(Locale.US).apply { minimumFractionDigits = 2 }.format(number / 100)
-}
-
-private fun volumeCreditsFor(perf: Performance): Int {
-    var result = 0
-    result += maxOf(perf.audience - 30, 0)
-    if ("comedy" == playFor(perf).type) result += perf.audience / 5
-
-    return result
-}
-
-private fun playFor(performance: Performance) = plays[performance.playID]!!
-
-private fun amountFor(performance: Performance): Int {
-    var result: Int
-
-    when (playFor(performance).type) {
-        "tragedy" -> { // 비극
-            result = 40000
-            if (performance.audience > 30) {
-                result += 1000 * (performance.audience - 30)
-            }
-        }
-
-        "comedy" -> { // 희극
-            result = 30000
-            if (performance.audience > 20) {
-                result += 10000 + 500 * (performance.audience - 20)
-            }
-            result += 300 * performance.audience
-        }
-
-        else -> {
-            throw IllegalArgumentException("알 수 없는 장르: ${playFor(performance).type}")
-        }
-    }
     return result
 }
