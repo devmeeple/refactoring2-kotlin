1358f30a7a49ae6eeded794f87350d0b8f4d7cf8
장태근 <devmeeple@gmail.com>
2025-01-23 13:57:41 +0900

    [1-8] 공연료 계산(amountFor) PerformanceCalculator로 옮기기
    - 함수 옮기기(Move Function)
    - 함수를 인라인(Inline Function)

--- src/main/kotlin/ch1/PerformanceCalculator.kt
+++ src/main/kotlin/ch1/PerformanceCalculator.kt
@@ -1,5 +1,29 @@
 package ch1
 
 class PerformanceCalculator(val performance: Performance, var play: Play) {
+    fun amount(): Int {
+        var result: Int
 
+        when (this.play.type) {
+            "tragedy" -> { // 비극
+                result = 40000
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30)
+                }
+            }
+
+            "comedy" -> { // 희극
+                result = 30000
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20)
+                }
+                result += 300 * this.performance.audience
+            }
+
+            else -> {
+                throw IllegalArgumentException("알 수 없는 장르: ${this.play.type}")
+            }
+        }
+        return result
+    }
 }
